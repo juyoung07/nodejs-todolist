59fea5fa79151fe97d4a3cc0990692c933ed804c
juyoung07 <yunju5090@gmail.com>
2025-01-29 23:56:51 +0900

    node todo start

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express")
 const app = express();
-app.listen(8080, () => {
-    console.log("listening on 8080")
+app.listen(3000, () => {
+    console.log("listening on 3000")
 })
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+let todos = []
+app.get("/", (req, res) => {
+    res.render("index", { todos: todos });
+});
