9d46b8bc83f332fae31b737690ab82081bdeee97
juyoung07 <yunju5090@gmail.com>
2025-02-11 22:49:05 +0900

    post 추가

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.listen(3000, () => {
     console.log("listening on 3000")
 })
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));   // 바디 파서 설정
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -12,3 +12,8 @@ let todos = []
 app.get("/", (req, res) => {
     res.render("index", { todos: todos });
 });
+
+app.post("/addTask", (req, res) => {
+    todos.push(req.body.add);
+    res.redirect("/");
+})
